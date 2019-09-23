7bdba756bb0049622c2d86f17846d4367b89543f
RK <30467445+Romekk@users.noreply.github.com>
2019-09-23 08:38:22 +0200

    Add fourth line and push manually

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -8,6 +8,8 @@ namespace Hello
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("First extra line ");
+            Console.WriteLine("Add third line at work on master branch ");
+            Console.WriteLine("Add fourth line");
         }
     }
 }
