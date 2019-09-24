20b1925e907847f516753da05d2f2e11fd7cdc9b
Romekk <roman.kaminski@hotmail.com>
2019-09-24 10:16:47 +0200

    sixth line

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -11,6 +11,7 @@ namespace Hello
             Console.WriteLine("Add third line at work on master branch ");
             Console.WriteLine("Add fourth line");
             Console.WriteLine("Add fifth line at home");
+            Console.WriteLine("Add sixth line 24.09");
         }
     }
 }
